72bc19067f0578ca21a8a72d7b6ede5ab1974e99
Ramanuj <75874452+Ramanuj-1729@users.noreply.github.com>
2022-12-25 22:18:03 +0530

    fix comment overflow

--- frontend/src/components/SingleComment/SingleComment.jsx
+++ frontend/src/components/SingleComment/SingleComment.jsx
@@ -14,12 +14,15 @@ const SingleComment = ({ comment }) => {
 
     return (
         <div className='comment-wrapper'>
-            <div className="comment mt-6 flex space-x-4">
-                <div className="avatar bg-gray1 w-10 h-10 rounded-full text-xl flex items-center justify-center text-white"><i className="fa-solid fa-user"></i></div>
+            <div style={{ overflowWrap: 'anywhere' }} className="comment mt-6 flex space-x-4">
+                <div className='avatar-wrapper'>
+                    <div className="avatar bg-gray1 w-10 h-10 rounded-full text-xl flex items-center justify-center text-white"><i className="fa-solid fa-user"></i>
+                    </div>
+                </div>
                 <div className="info">
                     <h3 className="userName font-poppins text-xl font-medium text-fontColor">{comment.userName}</h3>
                     <span className='dateAndTime font-normal text-sm font-poppins text-fontColor'>{`${month} ${date}, ${year} At ${hour}:${minute} ${ampm}`}</span>
-                    <p className=" commentBody my-2 font-poppins text-lg font-normal text-fontColor">{comment.comment}</p>
+                    <p className="commentBody my-2 font-poppins text-lg font-normal text-fontColor">{comment.comment}</p>
                     {/* <PrimaryButton width="1/2" height="8" buttonName="Reply" /> */}
                 </div>
             </div>
