86cb89b1b0ccfd1ba4a61d85aca7d2f7d1051884
Ramanuj <75874452+Ramanuj-1729@users.noreply.github.com>
2022-12-30 13:32:38 +0530

    set responsive screens

--- frontend/src/components/shared/Navbar/Navbar.jsx
+++ frontend/src/components/shared/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
-import { Link, NavLink, useLocation } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import axios from 'axios';
 import { useRef } from "react";
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,18 +3,25 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: "#FF3F59",
-        fontColor: "#282750",
-        white: "#ffffff",
-        gray1: "#e6e6e6",
-        gray2: "#a6a6a6",
-        gray3: "#333267",
-      },
-      fontFamily: {
-        'poppins': ['"Poppins"', 'sans-serif']
-      }
+    colors: {
+      primary: "#FF3F59",
+      fontColor: "#282750",
+      white: "#ffffff",
+      gray1: "#e6e6e6",
+      gray2: "#a6a6a6",
+      gray3: "#333267",
+    },
+    fontFamily: {
+      'poppins': ['"Poppins"', 'sans-serif']
+    },
+    screens: {
+      'smallPhone': '320px', // Phone - portrait
+      'largePhone': '480px', // Phone - landscape + portrait
+      'smallTablet': '600px', // Small tablet - portrait
+      'mediumTablet': '768px', // Large tablet - portrait
+      'largeTablet': '1024px', // Large tablet - landscape
+      'desktop': '1280px', // Desktop
+      'wideScreen': '1440px', // Wide Screen Desktop
     },
   },
   plugins: [],
