194168eaf30a95eac8e24746768dd34ffded86c7
Ramanuj <75874452+Ramanuj-1729@users.noreply.github.com>
2023-01-12 15:23:53 +0530

    create hameburger menu

--- frontend/src/components/shared/Navbar/Navbar.jsx
+++ frontend/src/components/shared/Navbar/Navbar.jsx
@@ -8,10 +8,12 @@ const Navbar = () => {
     const location = useLocation();
     let path = location.pathname;
     const active = "text-primary border-b-2 border-primary";
+    const activeHameburger = "text-white bg-primary";
 
     const [shadow, setShadow] = useState(false);
     const [categories, setCategories] = useState([]);
     const [dropDownActive, setDropDownActive] = useState(false);
+    const [hameburgerActive, setHameburgerActive] = useState(false);
 
     useEffect(() => {
         const fetchCategories = async () => {
@@ -47,7 +49,7 @@ const Navbar = () => {
 
     return (
         <nav className={`navbar sticky top-0 z-10 bg-white ${shadow === true ? "drop-shadow-xl" : ""} transition duration-300 ease-out`}>
-            <div className="navbar-wrapper max-w-[1536px] mx-auto desktop:px-36 largeTablet:px-20 mediumTablet:px-8 smallTablet:px-20 largePhone:px-8 smallPhone:px-4 flex items-center justify-between h-16">
+            <div className="navbar-wrapper max-w-[1536px] mx-auto desktop:px-36 largeTablet:px-20 mediumTablet:px-8 smallTablet:px-12 largePhone:px-8 smallPhone:px-4 flex items-center justify-between h-16">
                 <div className="logo flex-shrink-0">
                     <NavLink to="/"><h1 className='font-poppins font-black smallTablet:text-3xl smallPhone:text-2xl text-primary cursor-pointer'>TechBlog</h1></NavLink>
                 </div>
@@ -81,13 +83,45 @@ const Navbar = () => {
                     </ul>
                 </div>
 
-                <div className="small-screen-menu mediumTablet:hidden smallPhone:flex justify-center items-center space-x-8">
-                    <span className='search text-fontColor hover:text-primary px-1 py-2 text-sm font-semibold transition-all ease-in-out delay-150 cursor-pointer'><i className="fa-solid fa-magnifying-glass"></i></span>
-                    <span className="space-y-1 cursor-pointer z-10">
-                        <div className="bg-fontColor w-8 h-[3px] rounded-xl"></div>
-                        <div className="bg-fontColor w-6 h-[3px] rounded-xl"></div>
-                        <div className="bg-fontColor w-8 h-[3px] rounded-xl"></div>
-                    </span>
+                <div className="small-screen-menu mediumTablet:hidden smallPhone:flex justify-center items-center">
+                    <span className='search text-fontColor hover:text-primary px-1 py-2 mr-8 text-sm font-semibold transition-all ease-in-out delay-150 cursor-pointer'><i className="fa-solid fa-magnifying-glass"></i></span>
+                    <span className="cursor-pointer text-fontColor hover:text-primary text-xl transition-all ease-in-out delay-150" onClick={() => setHameburgerActive(true)}><i class="fa-solid fa-bars"></i></span>
+                </div>
+
+                <div className={`menu absolute h-screen largePhone:w-1/2 smallPhone:w-4/6 top-0 right-0 bottom-0 bg-white drop-shadow-2xl text-fontColor text-lg font-medium justify-center pt-20 ${hameburgerActive === true ? 'flex' : 'hidden'}`}>
+                    <i className="fa-solid fa-xmark cursor-pointer absolute top-5 right-12 text-fontColor hover:text-primary text-3xl transition-all ease-in-out delay-150" onClick={() => setHameburgerActive(false)}></i>
+                    <ul className="w-full transition-all ease-in-out delay-150">
+                        <NavLink to="/">
+                            <li className={`w-full px-5 py-3 my-2 cursor-pointer hover:text-white hover:bg-primary ${path === "/" ? activeHameburger : ""}`}>
+                                <i className="fa-solid fa-house"></i>
+                                <span className="ml-3">Home</span>
+                            </li>
+                        </NavLink>
+                        <NavLink to="/posts">
+                            <li className={`w-full px-5 py-3 my-2 cursor-pointer hover:text-white hover:bg-primary  ${path === "/posts" ? activeHameburger : ""}`}>
+                                <i className="fa-solid fa-pager"></i>
+                                <span className="ml-3">Articles</span>
+                            </li>
+                        </NavLink>
+                        <NavLink to="">
+                            <li className={`w-full px-5 py-3 my-2 cursor-pointer hover:text-white hover:bg-primary`}>
+                                <i className="fa-solid fa-table-cells-large"></i>
+                                <span className="ml-3">Category</span>
+                            </li>
+                        </NavLink>
+                        <NavLink to="/about">
+                            <li className={`w-full px-5 py-3 my-2 cursor-pointer hover:text-white hover:bg-primary  ${path === "/about" ? activeHameburger : ""}`}>
+                                <i className="fa-solid fa-info"></i>
+                                <span className="ml-3">About</span>
+                            </li>
+                        </NavLink>
+                        <NavLink to="/contact">
+                            <li className={`w-full px-5 py-3 my-2 cursor-pointer hover:text-white hover:bg-primary  ${path === "/contact" ? activeHameburger : ""}`}>
+                                <i className="fa-solid fa-phone"></i>
+                                <span className="ml-3">Contact</span>
+                            </li>
+                        </NavLink>
+                    </ul>
                 </div>
 
             </div>
