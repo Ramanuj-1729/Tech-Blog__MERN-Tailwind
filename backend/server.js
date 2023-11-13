a4d3bce1acba441c30b395373c09f6bfff299172
Ramanuj <75874452+Ramanuj-1729@users.noreply.github.com>
2023-11-13 19:44:52 +0530

    add corsOptions

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,12 @@ db.once('open', () => {
 });
 
 global.appRoot = path.resolve(__dirname);
-app.use(cors());
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true,
+    optionSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/api', routes);
